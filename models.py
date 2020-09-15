b19a673cb886efca73288f9bdff0e705374b8d71
Mauro MSL <mauro.msl@outlook.com>
2020-09-15 19:11:02 +0100

    Adds initial migration

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 class DOAJArticle(models.Model):
     article = models.OneToOneField("submission.Article")
     doaj_id = models.CharField(max_length=255)
-    exported = models.DateTimeField(default=timezone.now)
-    last_updated = models.DateTimeField(default=timezone.now)
+    exported = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         unique_together = ("article", "doaj_id")
