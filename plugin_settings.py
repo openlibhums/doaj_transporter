da14bf4bd442297f23271e4fe9e885ac2dcfd0e3
Mauro MSL <mauro.msl@outlook.com>
2021-02-21 20:23:54 +0000

    Improve installer to handle upgrades

--- plugin_settings.py
+++ plugin_settings.py
@@ -1,4 +1,5 @@
 from utils import models
+from utils.install import update_settings
 
 PLUGIN_NAME = 'doaj'
 DESCRIPTION = 'A plugin for exporting metadata to DOAJ via their API'
@@ -8,19 +9,27 @@ SHORT_NAME = 'DOAJ'
 DISPLAY_NAME = 'DOAJ'
 MANAGER_URL = 'doaj_index'
 
-DOAJ_API_TOKEN = ""
-
 
 def install():
-    new_plugin, created = models.Plugin.objects.get_or_create(
+    plugin, created = models.Plugin.objects.get_or_create(
         name=SHORT_NAME,
-        display_name=DISPLAY_NAME,
-        version=VERSION,
-        enabled=True
+        defaults={
+            "enabled": True,
+            "version": VERSION,
+            "display_name": DISPLAY_NAME,
+        }
     )
 
     if created:
         print('Plugin {0} installed.'.format(PLUGIN_NAME))
+        update_settings(
+            file_path='plugins/doaj_transporter/install/settings.json'
+        )
+    elif plugin.version != VERSION:
+        print('Plugin updated: {0} -> {1}'.format(VERSION, plugin.version))
+        update_settings(
+            file_path='plugins/doaj_transporter/install/settings.json'
+        )
     else:
         print('Plugin {0} is already installed.'.format(PLUGIN_NAME))
 
